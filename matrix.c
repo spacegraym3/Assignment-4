a699890cdcbbbf9d981a2a374d3dc0232a5b1d41
Christopher A White <spacegraym3@users.noreply.github.com>
2026-09-22 18:41:42 +0000

    Added generate matrix method as well as git ignore

--- matrix.c
+++ matrix.c
@@ -4,7 +4,15 @@
 #include <string.h>
 
 void generate_random_matrix(int rows, int cols, int *matrix) {
- 
+    if (matrix == NULL || rows <= 0 || cols <= 0) {
+        return;  // invalid input
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            matrix[i * cols + j] = rand();
+        }
+    }
 }
 
 void multiply_matrices(int rows1, int cols1, int *matrix1,
@@ -14,7 +22,12 @@ void multiply_matrices(int rows1, int cols1, int *matrix1,
 }
 
 void display_matrix(int rows, int cols, int *matrix) {
-   
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d\t", matrix[i * cols + j]);
+        }
+        printf("\n");
+    }
 }
 
 float do_job(int rows1, int cols1, int cols2, int forever) {
