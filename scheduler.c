550ad8373073579262ab905fcc55a4c0bbac0444
Christopher A White <spacegraym3@users.noreply.github.com>
2026-09-26 00:51:40 +0000

    commented out duplicate code

--- scheduler.c
+++ scheduler.c
@@ -24,18 +24,4 @@ float FIFO(int* jobs, int size) {
 // Shortest job first
 float SJF(int* jobs, int size) {
     return FIFO(jobs, size);
-    /*
-    if (size <= 0) return 0.0f;
-
-    float totalTime = 0;
-    for (int i = 0; i < size; i++) {
-        int number = (int)jobs[i];
-        
-        float elapsed = do_job(number, number, number, 0);
-        totalTime += elapsed;
-        printf("%dx%d matrices: %.6f seconds\n", number, number, elapsed);
-    }
-    printf("Average time: %f\n", (float)(totalTime/size));
-    return totalTime;
-    */
 }
